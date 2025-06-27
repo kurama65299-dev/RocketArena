1f848855f5b0e770074aaee4122e40fcdf94fe6a
bobad_augusto-dev <bobadillaaugusto3@gmail.com>
2025-06-27 18:53:42 -0300

    Added timer

---
+++ scenes/game.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@onready var end_game: Timer = $EndGame
+@onready var label: Label = $EndGame/Label
+
+func _ready():
+	end_game.start()
+	
+func _process(delta):
+	timer_ui(delta)
+
+var seconds_passed = 0
+func timer_ui(delta):
+	seconds_passed += delta
+	if seconds_passed >= 1:
+		seconds_passed = 0
+		label.text = "Time: " + str(round(end_game.time_left))
+	
+func _on_end_game_timeout() -> void:
+	GlobalSettings.end_game()
--- scripts/main_menu.gd
+++
@@ -1,5 +0,0 @@
-extends CanvasLayer
-
-
-func _on_spiky_battle_button_down() -> void:
-	pass # Replace with function body.
