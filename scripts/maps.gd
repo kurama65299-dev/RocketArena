d4a449711606cd2ccdab4c8786b8ead29bc7bed2
bobad_augusto-dev <bobadillaaugusto3@gmail.com>
2025-06-28 12:24:47 -0300

    Minor redesign and renaming

--- scripts/maps.gd
+++ scripts/maps.gd
@@ -1,5 +1,5 @@
 extends Panel
-@onready var maps: Panel = $"."
+@onready var game_settings: Panel = $"."
 @onready var players: Panel = $"../Players"
 
 func _on_spiky_battle_button_down() -> void:
@@ -10,7 +10,7 @@ func _on_descending_war_button_down() -> void:
 
 func _on_next_button_down() -> void:
 	players.visible = true
-	maps.visible = false
+	game_settings.visible = false
 
 func _on_gamemode_item_selected(index: int) -> void:
 	if index == 0:
--- scripts/play_button.gd
+++ scripts/play_button.gd
@@ -1,8 +1,8 @@
 extends Button
 
 @onready var play_settings: Panel = $"../../PlaySettings"
-@onready var maps: Panel = $"../../PlaySettings/Maps"
+@onready var game_settings: Panel = $"../../PlaySettings/GameSettings"
 
 func _on_button_down() -> void:
 	play_settings.visible = true
-	maps.visible = true
+	game_settings.visible = true
